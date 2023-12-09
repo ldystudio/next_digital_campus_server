7e6c50facf56428208794b72ef4c6d906eb9ed88
Liudy <1187551003@qq.com>
2023-12-09 22:00:20 +0800

    ✨ feat(登录): 添加邮箱登录

--- common/backends.py
+++ common/backends.py
@@ -9,49 +9,28 @@ from iam.models import User
 # 自定义身份校验
 class AuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        traceId = request.data.get('traceId')
+        trace_id = request.data.get('traceId')
         login_type = request.data.get('type')
-        if login_type == 'emailLogin':
-            verCode = request.data.get('emailCaptcha')
-            email = request.data.get('email')
-            if verCode is None or traceId is None:
-                raise serializers.ValidationError('验证码不能为空')
+        email = request.data.get('email')
 
-            # 获取缓存中的验证码
-            captcha = cache.get(traceId, version='EmailCaptcha')
-            # 验证码验证
-            if verCode.lower() != captcha.lower():
-                raise serializers.ValidationError('验证码错误')
-            # 验证码验证成功后，删除缓存
-            # cache.expire(traceId, timeout=0, version='EmailCaptcha')
+        # if login_type not in ['emailLogin', 'imageLogin']:
+        #     raise serializers.ValidationError('Invalid login type')
 
-            try:
-                # 用户名和邮箱验证
-                user = User.objects.get(email=email)
-            except User.DoesNotExist:
-                raise serializers.ValidationError('邮箱没有注册')
-            return user
-        else:
-            verCode = request.data.get('captcha')
-            if verCode is None or traceId is None:
-                raise serializers.ValidationError('验证码不能为空')
+        ver_code = request.data.get('emailCaptcha' if login_type == 'emailLogin' else 'captcha')
+        if ver_code is None or trace_id is None:
+            raise serializers.ValidationError('验证码不能为空')
 
-            # 获取缓存中的验证码
-            captcha = cache.get(traceId, version='ImageCaptcha')
-            # 验证码验证
-            if not captcha or verCode.lower() != captcha.lower():
-                raise serializers.ValidationError('验证码错误')
-            # 验证码验证成功后，删除缓存
-            cache.expire(traceId, timeout=0, version='ImageCaptcha')
+        captcha = cache.get(trace_id, version='EmailCaptcha' if login_type == 'emailLogin' else 'ImageCaptcha')
+        if not captcha or ver_code.lower() != captcha.lower():
+            raise serializers.ValidationError('验证码错误')
+        cache.expire(trace_id, timeout=0, version='EmailCaptcha' if login_type == 'emailLogin' else 'ImageCaptcha')
 
-            try:
-                # 用户名和邮箱验证
-                user = User.objects.get(Q(username=username) | Q(email=username))
-            except User.DoesNotExist:
-                raise serializers.ValidationError('账号没有注册')
+        try:
+            user = User.objects.get(Q(username=username) | Q(email=username) | Q(email=email))
+        except User.DoesNotExist:
+            raise serializers.ValidationError('账号没有注册')
 
-            if user.check_password(password):
-                return user
-            else:
-                # 密码错误，送给SimpleJwt报错
-                return
+        if login_type != 'emailLogin' and not user.check_password(password):
+            raise serializers.ValidationError('账号或密码错误')
+
+        return user
