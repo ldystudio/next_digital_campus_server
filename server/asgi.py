a700f327123a73fc66e3b0f35599f1d4c2f847c1
Liudy <1187551003@qq.com>
2024-05-16 02:18:41 +0800

    ✨ feat(new): 2024-05-15

--- server/asgi.py
+++ server/asgi.py
@@ -11,9 +11,7 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from channels_auth_token_middlewares.middleware import (
-    SimpleJWTAuthTokenMiddlewareStack,
-)
+from channels_auth_token_middlewares.middleware import SimpleJWTAuthTokenMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 from chat.routing import websocket_urlpatterns
