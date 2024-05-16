933dafc65e41e143db8d14375b4a9f168f2f8f1c
Liudy <785861952@qq.com>
2024-05-16 16:46:17 +0800

    asgi相关修改

--- server/asgi.py
+++ server/asgi.py
@@ -9,15 +9,16 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from channels_auth_token_middlewares.middleware import SimpleJWTAuthTokenMiddlewareStack
 from django.core.asgi import get_asgi_application
-
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels_auth_token_middlewares.middleware import SimpleJWTAuthTokenMiddlewareStack
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
